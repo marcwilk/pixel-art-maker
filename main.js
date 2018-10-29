777ac9747a95b7edb63a7d62c2b8a9930b29a7a8
marcwilk <42052120+marcwilk@users.noreply.github.com>
2018-10-29 14:30:14 -0600

    Added files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let color = document.getElementById("colorPicker");
+let table = document.getElementById("pixelCanvas");
+let sizePicker = document.getElementById("sizePicker");
+
+let height = document.getElementById("inputHeight").value;
+let width = document.getElementById("inputWeight").value;
+makeGrid(height, width);
+
+sizePicker.addEventListener("click", (e) => {
+
+  e.preventDefault();
+
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWeight").value;
+  table.firstElementChild.remove();
+
+  makeGrid(height, width);
+
+});
+
+function makeGrid(height, width){
+
+	for(let i=0; i< height; i++){
+		let row = table.insertRow(i);
+		for(let j=0; j< width; j++){
+			let cell = row.insertCell(j);
+			cell.addEventListener("click", (e) => {
+				cell.style.backgroundColor = color.value;
+			})
+		}
+	}
+
+}
